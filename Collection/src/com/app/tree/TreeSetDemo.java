723d23b87d4cb889134b8ed92f225f1a2573215f
santosh kumar <santosh kumar@192.168.2.13>
2017-08-10 23:31:15 +0530

    second time modify

--- Collection/src/com/app/tree/TreeSetDemo.java
+++ Collection/src/com/app/tree/TreeSetDemo.java
@@ -3,23 +3,27 @@ package com.app.tree;
 import java.util.TreeSet;
 
 public class TreeSetDemo {
-public static void main(String[] args) {
-	TreeSet t=new TreeSet();
-	System.out.println("A".compareTo("Z"));
-	System.out.println("Z".compareTo("K"));
-	System.out.println("A".compareTo("A"));
-	//System.out.println("A".compareTo(null));
-/*t.add("K");
-t.add("Z");
-t.add("A");
-t.add("A");
-System.out.println(t);*/
-t.add(0);
-t.add(10);
-t.add(5);
-t.add(15);
-System.out.println(t);
-System.out.println(t);
-System.out.println(t);
-}
+	public static void main(String[] args) {
+		TreeSet t = new TreeSet();
+		System.out.println("A".compareTo("Z"));
+		System.out.println("Z".compareTo("K"));
+		System.out.println("A".compareTo("A"));
+		// System.out.println("A".compareTo(null));
+		/*
+		 * t.add("K"); t.add("Z"); t.add("A"); t.add("A");
+		 * System.out.println(t);
+		 */
+		t.add(0);
+		t.add(10);
+		t.add(5);
+		t.add(15);
+		System.out.println(t);
+		System.out.println(t);
+		System.out.println(t);
+		System.out.println(t.getClass().getName());
+		System.out.println(t.hashCode());
+		System.out.println(t.isEmpty());
+		System.out.println(t.size());
+		System.out.println(t.first());
+	}
 }
